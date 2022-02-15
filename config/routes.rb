4e7b2263652b6f9f828c9381b12d24295e194f67
alex hall <alex_hall@hotmail.co.uk>
2022-02-15 12:27:09 +0000

    Model added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #create
+  get '/task/new', to: 'task#new'
+  post '/task', to: 'task#create'
+  #read
+  get '/task', to: 'task#index'
+  get '/task/:id', to: 'task#show'
+  #update
+  get '/task/:id/edit', to: 'task#edit'
+  patch '/task/:id', to: 'task#update'
+  #delete
+  delete '/task/:id', to: 'task#destroy'
 end
